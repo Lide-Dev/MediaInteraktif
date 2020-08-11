6a7bd3db7ae6a67c47da435915647ab1d3527df4
thetarius26 <rhakadewanto26@gmail.com>
2020-08-12 01:34:34 +0700

    Add:  SplashActivity  LoginActivity  RegisterActivity  KompetensiActivity  HomeActivity (Navigation Bottom)  NavBotFragment

---
+++ app/src/main/java/com/mediainteraktif/ui/profile/ProfileFragment.kt
@@ -0,0 +1,19 @@
+package com.mediainteraktif.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mediainteraktif.R
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        return root
+    }
+}
