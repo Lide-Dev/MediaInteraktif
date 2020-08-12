a160548323474fe169a0303657d219da82dc4541
TheTarius26 <rhakadewanto26@gmail.com>
2020-08-13 04:06:51 +0700

    SplashActivity.kt :  Button Logic  1 Bug Fix

--- app/src/main/java/com/mediainteraktif/SplashActivity.kt
+++ app/src/main/java/com/mediainteraktif/SplashActivity.kt
@@ -2,13 +2,12 @@ package com.mediainteraktif
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
 
