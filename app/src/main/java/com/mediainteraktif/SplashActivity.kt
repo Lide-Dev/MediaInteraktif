9c66f8b9cddb29c9956aca743c246e69963a10f7
TheTarius26 <rhakadewanto26@gmail.com>
2020-08-13 03:52:19 +0700

    SplashActivity.kt :  Button Logic

--- app/src/main/java/com/mediainteraktif/SplashActivity.kt
+++ app/src/main/java/com/mediainteraktif/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.mediainteraktif
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        val btnStart = findViewById<LinearLayout>(R.id.splash_btn_start)
+
+        btnStart.setOnClickListener {
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+        }
     }
 }
